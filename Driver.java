c848fd262f7a285fb4f0b8105a491185d6c4e5e6
Mohammad Jawwad <mjawwad@stuy.edu>
2018-10-03 19:08:03 -0400

    commented out deposit test case and replaced with another to test failure feature of withdrawal, spaced out / easier reading of printed lines

--- Driver.java
+++ Driver.java
@@ -3,21 +3,22 @@ public class Driver {
     BankAccount a = new BankAccount(5429, 5000, "pass");
 
     System.out.println("The account number is " + a.getAccountID());
-    System.out.println("The current balance of this account is" + a.getBalance());
+    System.out.println("The current balance of this account is " + a.getBalance());
     System.out.println(a);
 
-    if( a.withdraw(1000) ){
+    //if( a.withdraw(1000) ){
+    if( a.withdraw(9000)) {
       System.out.println("Withdrawal success!");
     }
     else {System.out.println("Withdrawal failure");}
 
-    System.out.println("The current balance of this account is" + a.getBalance());
+    System.out.println("The current balance of this account is " + a.getBalance());
 
     if( a.deposit(7000) ){
              System.out.println("Deposit success!");
         }
         else {System.out.println("Deposit failure");}
 
-    System.out.println(a.getBalance() + "is the current balance of this account" + a.getBalance());
+    System.out.println("The current balance of this account is " + a.getBalance());
   }
 }
